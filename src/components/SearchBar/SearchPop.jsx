5f52c8bec301e463e53511f4dc8d76119383ef33
Ancy Sherin A <ancy.a01@ad.infosys.com>
2025-07-14 23:27:34 +0530

    li-fix

--- src/components/SearchBar/SearchPop.jsx
+++ src/components/SearchBar/SearchPop.jsx
@@ -1,49 +1,45 @@
+import React from 'react';
 import Button from '../Button/Button';
 import "./SearchPop.css";
 
-const SearchPop = props => {
-    const { locations, clickFunction, hospitals, atBookingsPage } = props;
+const SearchPop = ({ locations, clickFunction, hospitals, atBookingsPage }) => {
+  const displayItems = () => {
+    if (atBookingsPage) {
+      if (!hospitals?.length) return null;
+      return hospitals.map((item, index) => {
+        const { hospitalName, county, city, rating, hospitalType } = item.data;
+        const { time, date } = item.dateTime;
 
-    const displayStates = () => {
-        if (atBookingsPage) {
-            if (!hospitals?.length) return null;
+        return (
+          <li key={index} className='SearchPopItem SearchPopItem-bookings'>
+            <span>{hospitalName}</span>
+            <span className='resultContent-right resultContent-top'>
+              <Button text={time} buttonClass="smallButton blueButton-outlined" />
+              <Button text={date} buttonClass="smallButton greenButton-outlined" />
+            </span>
+          </li>
+        );
+      });
+    }
 
-            return hospitals.map((item, index) => {
-                const { hospitalName, county, city, rating, hospitalType } = item.data;
-                const { time, date } = item.dateTime;
-                return (
-                    <li key={index} className='SearchPopItem SearchPopItem-bookings'>
-                        <span>{hospitalName}</span>
-                        <span className='resultContent-right resultContent-top'>
-                            <Button text={time} buttonClass={`smallButton blueButton-outlined`} />
-                            <Button text={date} buttonClass={`smallButton greenButton-outlined`} />
-                        </span>
-                    </li>
-                );
-            });
-        }
+    if (!locations?.length) return null;
 
-        if (!locations?.length) return null;
+    return locations.map((item, index) => (
+      <li
+        key={index}
+        onClick={() => clickFunction(item)}
+        //className='SearchPopItem'
+      >
+        {item}
+      </li>
+    ));
+  };
 
-        return locations.map((item, index) => (
-            <li
-                key={index}
-                onClick={() => clickFunction(item)}
-                className='SearchPopItem'
-                role="option"
-            >
-                {item}
-            </li>
-        ));
-    };
-
-    return (
-        <div className='SearchPop'>
-            <ul>
-                {displayStates()}
-            </ul>
-        </div>
-    );
+  return (
+    <ul className='SearchPop'>
+      {displayItems()}
+    </ul>
+  );
 };
 
 export default SearchPop;
