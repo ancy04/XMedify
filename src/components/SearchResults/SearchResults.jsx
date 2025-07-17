ebb4c3511c25cf7654da0698d9c511a8284c8d87
Ancy Sherin A <ancy.a01@ad.infosys.com>
2025-07-17 14:25:39 +0530

    cityName lowercase

--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -41,7 +41,7 @@ const SearchResults = () => {
         <div className='SearchResults' >
             <div className='commonContainer resultsBody'>
                 <div className='resultsHead'>
-                    <h1>{foundHospitals.noSearchYet ? headline0 : `${foundHospitals?.hospitals?.length} medical centers available in ${foundHospitals?.cityName}`}</h1>
+                    <h1>{foundHospitals.noSearchYet ? headline0 : `${foundHospitals?.hospitals?.length} medical centers available in ${foundHospitals?.cityName?.toLowerCase()}`}</h1>
                     <p>
                         <img src={checkIcon} alt='check icon' className='checkIcon'/>
                         <span>{subText}</span>
